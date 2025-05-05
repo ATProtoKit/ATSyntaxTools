7cff353d71c28749e1ede3b47e8092f52f52bea6
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-05-05 18:42:54 -0400

    Update Package.swift
    - Downgraded swift-tools-version to Swift 5.9.
    - Added Package.swift specific to Swift 6.0+.

--- Package@6.0.0.swift
+++ Package@6.0.0.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -29,16 +29,3 @@ let package = Package(
         ),
     ]
 )
-
-let swiftSettings: [SwiftSetting] = [
-    .enableExperimentalFeature("StrictConcurrency"),
-    .enableUpcomingFeature("DisableOutwardActorInference"),
-    .enableUpcomingFeature("GlobalActorIsolatedTypesUsability"),
-    .enableUpcomingFeature("InferSendableFromCaptures"),
-]
-
-for target in package.targets {
-    var settings = target.swiftSettings ?? []
-    settings.append(contentsOf: swiftSettings)
-    target.swiftSettings = settings
-}

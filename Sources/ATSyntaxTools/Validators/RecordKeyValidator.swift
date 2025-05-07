7a9b86c016a9419ffd3fb494a496bd8f753eb395
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-05-07 04:16:15 -0400

    Create RecordKeyValidator

---
+++ Sources/ATSyntaxTools/Validators/RecordKeyValidator.swift
@@ -0,0 +1,36 @@
+//
+//  RecordKeyValidator.swift
+//  ATSyntaxTools
+//
+//  Created by Christopher Jr Riley on 2025-05-05.
+//
+
+import Foundation
+
+/// Identifies and validates Record Keys.
+public enum RecordKeyValidator: ValidatorProtocol {
+
+    /// Ensures the Record Key is valid.
+    ///
+    /// - Parameter recordKey: The record key to be validated.
+    ///
+    /// - Throws: ``InvalidRecordKeyError`` , indicating the Record Key is invalid.
+    public static func validate(_ recordKey: String) throws {
+        guard recordKey.count > 1, recordKey.count <= 512 else {
+            throw InvalidRecordKeyError.invalidLength
+        }
+
+        let characterCheck = CharacterSet.decimalDigits
+            .union(.uppercaseLetters)
+            .union(.lowercaseLetters)
+            .union(CharacterSet(charactersIn: "_~.:-"))
+
+        guard recordKey.rangeOfCharacter(from: characterCheck.inverted) == nil else {
+            throw InvalidRecordKeyError.invalidSyntax
+        }
+
+        guard recordKey != ".", recordKey != ".." else {
+            throw InvalidRecordKeyError.onlyContainsPeriods
+        }
+    }
+}

c3e6efb16e7b61fec68db31b9ef4283ef08bc638
Christopher Jr Riley <chrisjr@cjrriley.com>
2026-07-01 16:13:40 -0400

    Add helper files
    This is coming straight from the atproto repo. The tags are used to help
    categorize the types of tests.

---
+++ Tests/ATSyntaxToolsTests/Helpers/TagExtension.swift
@@ -0,0 +1,17 @@
+//
+//  TagExtension.swift
+//  ATSyntaxTools
+//
+//  Created by Christopher Jr Riley on 2026-07-01.
+//
+
+import Testing
+
+extension Tag {
+    @Tag static var handles: Self
+    @Tag static var dids: Self
+    @Tag static var nsids: Self
+    @Tag static var recordKeys: Self
+    @Tag static var tids: Self
+    @Tag static var atURIs: Self
+}
